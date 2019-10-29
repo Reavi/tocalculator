85e53218eb5c229227643cd2c328a691d57ff3eb
Dawid Koziej <januszakahipis@wp.pl>
2019-10-29 12:54:30 +0100

    add powershell scrypt to run mvn app

---
+++ winStart.ps1
@@ -0,0 +1,11 @@
+if (Test-Path ".\calculator-main\target\calculator-main-1.0-SNAPSHOT.jar"){
+	java -cp calculator-main/target/calculator-main-1.0-SNAPSHOT.jar pl.calculator.App
+}
+else {
+	Write-Warning "Nie wykryto pliku. Instalacja..."
+	mvn clean install
+	echo "Nacisnij dowolny przycisk aby kontynuowac i wyczyscic konsole"
+	$KeyPress = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	clear
+	java -cp calculator-main/target/calculator-main-1.0-SNAPSHOT.jar pl.calculator.App
+}

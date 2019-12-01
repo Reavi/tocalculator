3d805aabef13c3c7bac78a785eead3d18503030a
Reavi <krystianmielec@gmail.com>
2019-12-01 13:30:30 +0100

    fixed loader plugin

--- calculator-core/src/main/java/pl/calculator/plugins/LoaderPlugin.java
+++ calculator-core/src/main/java/pl/calculator/plugins/LoaderPlugin.java
@@ -1,56 +1,56 @@
 package pl.calculator.plugins;
 
-import pl.calculator.Calculator;
+
 import pl.calculator.Operation;
 
-import java.io.File;
+import java.io.*;
 import java.lang.reflect.Constructor;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class LoaderPlugin {
-    //Coś co przechowuje ile pluginow jest załadowanych / nazwy? licbza?
     private ArrayList<URL[]> urls=new ArrayList<>();
     public LoaderPlugin() {
     }
     public Map<String,Operation> load() throws Exception{
         File [] listOfFiles=loadFromFile();
-        URL[] classLoaderUrls = new URL[listOfFiles.length];
         Map<String,Operation> hp=new HashMap<>();
+
         for(File file : listOfFiles){
-            classLoaderUrls = new URL[]{new URL("file:///d:/plugin/"+file.getName())};
-            URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls,Thread.currentThread().getContextClassLoader());
-            //System.out.println(file.getName().substring(0,file.getName().length()-4));
-            Class cl = urlClassLoader.loadClass(file.getName().substring(0,file.getName().length()-4));
-            Constructor constructor = cl.getConstructor();
-            System.out.println("Plugin class name: "+constructor.getName());
-            Operation ic = (Operation) constructor.newInstance();
-            System.out.println("Plugin  operand: "+ic.getSign());
-            hp.put(ic.getSign(),ic);
-
-
+            ArrayList<String> names=getNamesClass(file);
+            for(String name : names){
+                URL[] classLoaderUrls = new URL[]{new URL("file:///d:/plugin/"+file.getName())};
+                URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls,Thread.currentThread().getContextClassLoader());
+                Class<?> cl = urlClassLoader.loadClass(name);
+                Constructor<?> constructor = cl.getConstructor();
+                System.out.print("Plugin class name: "+constructor.getName());
+                Operation ic = (Operation) constructor.newInstance();
+                System.out.println(", operand: "+ic.getSign());
+                hp.put(ic.getSign(),ic);
+            }
         }
-        /*
-        // Create a new URLClassLoader
-        URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls,Thread.currentThread().getContextClassLoader());
-
-
-        // z refleksją
-        Class cl = urlClassLoader.loadClass("Mul");
-        Constructor constructor = cl.getConstructor();
-        System.out.println("Plugin class name: "+constructor.getName());
-        Operation ic = (Operation) constructor.newInstance();
-        System.out.println("Plugin  operand: "+ic.getSign());
-*/
-
         return hp;
     }
     private File[] loadFromFile(){
         File folder = new File("d:/plugin/");
         return folder.listFiles(
                 (dir, name) -> name.toLowerCase().endsWith(".jar"));
+    }
+    private ArrayList<String> getNamesClass(File file) throws IOException {
+        JarFile jf = new JarFile(file);
+        Enumeration<JarEntry> entries = jf.entries();
+        String name = "";
+        ArrayList<String> names = new ArrayList<>();
+        while(entries.hasMoreElements()) {
+            name = entries.nextElement().getName();
+            if(name.endsWith(".class")){
+                name = name.substring(0,name.length()-6);
+                names.add(name);
+            }
+        }
+        return names;
     }
 }

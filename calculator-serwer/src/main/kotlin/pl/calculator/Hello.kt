1a20ab1e631f197ed9216fb88bfcaff65ff06809
Reavi <reavi20@gmail.com>
2019-12-14 13:14:29 +0100

    serwer heelo

--- calculator-serwer/src/main/kotlin/pl/calculator/Hello.kt
+++ calculator-serwer/src/main/kotlin/pl/calculator/Hello.kt
@@ -1,6 +1,17 @@
 package pl.calculator
 
+import io.vertx.core.Vertx
+
+import io.vertx.kotlin.core.http.HttpServerOptions
+
 fun main(args: Array<String>) {
-    println("Hello, World")
+
+    val vertx= Vertx.vertx()
+    val options = HttpServerOptions(
+            maxWebsocketFrameSize = 1000000)
+    vertx.createHttpServer(options).requestHandler({ request ->
+        request.response().end("Hello world")
+    }).listen(8080)
+
 }
 
